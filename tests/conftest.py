e684dae5d16d617f565a65b8a40d3dbf1f84f33b
User - Antoine REBILLARD <antoine.rebillard@hardis-group.com>
2024-11-14 17:38:57 +0100

    correct linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,14 @@
 import sys
 from pathlib import Path
+
 sys.path.append(str(Path(__file__).parent.parent / "app"))
 
 import pytest
 from fastapi.testclient import TestClient
 
 
-
 from app.main import app
+
 
 @pytest.fixture(scope="session")
 def client():
